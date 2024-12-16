569708045be636090a088bd390a80bd3614079c4
Shubham <Shubhampancahm796@gmail.com>
2024-12-16 19:45:29 +0530

    test feature

--- featureA.java
+++ featureA.java
@@ -2,4 +2,8 @@ public class featureA {
     public static void main(String[] args) {
         System.out.println("featur text");
     }
+    void test(){
+        System.out.println("test feature");
+    }
+
 }
